34e49a9956e531ad255fb41f9fb780f28bc3b8c8
ilia-che <ilia.1111@yandex.ru>
2021-10-24 23:35:56 +0300

    add ProductPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,9 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
+class ProductPageLocators():
+    ADD_PRODUCT_TO_BASKET_BTN = (By.CSS_SELECTOR, ".btn-add-to-basket")  # кнопка добавить в корзину
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")  # название товара
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")  # цена товара
+    PRODUCT_ADDED_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, ".alert:first-of-type strong")  # сообщение о дабавлении товара в корзину
+    PRODUCT_ADDED_PRICE_MESSAGE = (By.CSS_SELECTOR, ".alertinner p strong")  # сообщение о цене товаров в корзине
