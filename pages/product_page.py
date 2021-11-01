71b57b4c911fc43f6ee782f67b7fbba62c8158e4
ilia-che <ilia.1111@yandex.ru>
2021-11-01 18:40:34 +0300

    add should_not_be_success_message and should_be_disappeared_success_message

--- pages/product_page.py
+++ pages/product_page.py
@@ -20,3 +20,11 @@ class ProductPage(BasePage):
         product_name_in_message = self.element_text(*ProductPageLocators.PRODUCT_ADDED_TO_BASKET_MESSAGE)
         print(product_name_in_message)
         assert product_name == product_name_in_message, "Incorrect name of product in message"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is not disappeared, but should be"
