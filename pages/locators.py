a9e6d1d5b0e4f774acfbcb7f65f3a19ee421df11
ilia-che <ilia.1111@yandex.ru>
2021-11-01 18:25:07 +0300

    add SUCCESS_MESSAGE locator

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,4 @@ class ProductPageLocators():
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".product_main .price_color")  # цена товара
     PRODUCT_ADDED_TO_BASKET_MESSAGE = (By.CSS_SELECTOR, ".alert:first-of-type strong")  # сообщение о дабавлении товара в корзину
     PRODUCT_ADDED_PRICE_MESSAGE = (By.CSS_SELECTOR, ".alertinner p strong")  # сообщение о цене товаров в корзине
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, "#messages div:nth-child(1)")  # сообщение о успешном добавлении товара в корзину
