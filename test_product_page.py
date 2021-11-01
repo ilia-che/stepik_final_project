335618c4a9fb7784b34dbfeebfd6932aad179f01
ilia-che <ilia.1111@yandex.ru>
2021-11-01 18:43:27 +0300

    add 3 new tests

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,27 @@ from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 import pytest
 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                        # открываем страницу
+    page.add_product_to_basket()       # добавляем товар в корзину
+    page.should_not_be_success_message()
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                        # открываем страницу
+    page.should_not_be_success_message()
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                        # открываем страницу
+    page.add_product_to_basket()       # добавляем товар в корзину
+    page.should_be_disappeared_success_message()
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
