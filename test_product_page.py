13662d41f71e116851ed1a71676a608719ed26f1
ilia-che <ilia.1111@yandex.ru>
2021-11-04 21:49:48 +0300

    delete add_product_to_basket from test_guest_cant_see_product_in_basket_opened_from_main_page

--- test_product_page.py
+++ test_product_page.py
@@ -62,7 +62,6 @@ def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
-    page.add_product_to_basket()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
