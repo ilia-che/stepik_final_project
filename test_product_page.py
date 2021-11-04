c445cb05f12b735d4c309dd08b6a133bb83e3e88
ilia-che <ilia.1111@yandex.ru>
2021-11-04 21:42:47 +0300

    add new tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,4 +1,6 @@
 from .pages.main_page import MainPage
+from .pages.main_page import LoginPage
+from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 import pytest
 
@@ -41,3 +43,26 @@ def test_guest_can_add_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()     # решаем новогодний квиз
     page.should_be_same_price()        # проверяем правильность цены товара в сообщении
     page.should_be_same_name()         # проверяем правильность названия товара в сообщении
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
