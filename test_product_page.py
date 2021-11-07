075e583258ce3fcc5117bcabbd3a769fd108bd06
ilia-che <ilia.1111@yandex.ru>
2021-11-07 19:35:53 +0300

    Add new class TestUserAddToBasketFromProductPage wh 2 new tests: test_user_cant_see_success_message, test_user_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ from .pages.main_page import LoginPage
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 import pytest
+import time
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -58,10 +59,37 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
-def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty_basket()
+
+
+class TestUserAddToBasketFromProductPage():
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self,browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        email = str(time.time()) + "@fakemail.org"
+        password = "tester007"
+        page = LoginPage(browser,link)
+        page.open()
+        page.register_new_user(email, password)
+        page.should_be_authorized_user()
+
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser,link)
+        page.open()
+        page.should_not_be_success_message()
+
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+        page = ProductPage(browser,link)
+        page.open()
+        page.add_product_to_basket()
+        page.should_be_same_price()
+        page.should_be_same_name()
